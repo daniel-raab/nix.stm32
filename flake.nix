5fb8f9bf8a6cc4a2786e7f795162c04660fa0c26
Daniel Raab <daniel.raab@mailbox.org>
2024-03-17 18:01:08 +0100

    blinky working!
    added code from STM32CubeMX project
    STM32CubeMX generates a basic Makefile project with all libraries

--- flake.nix
+++ flake.nix
@@ -7,21 +7,24 @@
     let
       pkgs = import nixpkgs { system = "x86_64-darwin";};
       system = "x86_64-darwin";
-      buildTools = [pkgs.gcc-arm-embedded-13 ];
-      devTools = [pkgs.just];
+      buildTools = [pkgs.gcc-arm-embedded-13 pkgs.python311Packages.compiledb ];
+      devTools = [
+        pkgs.just
+        pkgs.helix
+      ];
     in
     {
       devShells.${system}.default = pkgs.mkShell {
         packages = buildTools ++ devTools;
       };
-      packages.${system}.default = pkgs.stdenv.mkDerivation {
+      packages.${system}.default = pkgs.clangSdtdenv.mkDerivation {
         name = "nix.stm32";
         packages = buildTools;
         src = self;
-        buildPhase = "make build";
+        buildPhase = "make";
         installPhase = ''
           mkdir -p $out/bin
-          cp build/blinky.elf $out/bin
+          cp build/nix.stm32.elf $out/bin
         '';
       };
     };
