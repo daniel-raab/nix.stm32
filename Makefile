5ae40efcb4c587529d75b6e00ac9952721756669
Daniel Raab <daniel.raab@mailbox.org>
2024-03-17 15:49:54 +0100

    flake can build the package and produce a shell
    `nix build` can be used to build the package (still hello world)
    `nix develop` can be used to enter a shell with the build tools and
    some additional development tools installed

---
+++ Makefile
@@ -0,0 +1,5 @@
+
+build: hello.c
+	mkdir -p build/
+	cc -o build/blinky.elf ./hello.c
+
--- flake.nix
+++ flake.nix
@@ -1,18 +1,28 @@
 {
   description = "Build a STM32 C project with Nix.";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
-    defaultPackage.x86_64-darwin =
-    with import nixpkgs { system = "x86_64-darwin"; };
-    stdenv.mkDerivation {
-      name = "hello";
-      src = self;
-      buildPhase = "cc -o hello ./hello.c";
-      installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
+  outputs = { self, nixpkgs }: 
+    let
+      pkgs = import nixpkgs { system = "x86_64-darwin";};
+      system = "x86_64-darwin";
+      buildTools = [pkgs.gcc-arm-embedded-13 ];
+      devTools = [pkgs.just];
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = buildTools ++ devTools;
+      };
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        name = "nix.stm32";
+        packages = buildTools;
+        src = self;
+        buildPhase = "make build";
+        installPhase = ''
+          mkdir -p $out/bin
+          cp build/blinky.elf $out/bin
+        '';
+      };
     };
-  };
 }
